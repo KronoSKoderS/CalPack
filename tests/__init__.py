d4cbe09017938cf491f12662eae872250925fd2f
kronos <superuser.kronos@gmail.com>
2017-08-03 10:23:44 -0700

    relative importing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,13 @@
 import unittest
 
-from tests.runtests import BasicPackets
-
 def get_tests():
     return full_suite()
 
 
 def full_suite():
-    
+
+    from .runtests import BasicPackets
+
     return unittest.TestSuite([
         unittest.TestLoader().loadTestsFromTestCase(BasicPackets),
     ])
