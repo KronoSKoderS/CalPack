63f22952ccd20baa71bda251a7ca67ca4f1659b2
Kronos <superuser.kronos@gmail.com>
2017-09-24 21:07:52 -0700

    updated to include unsigned and signed integers; also added to binary and to and from binary tests.

--- tests/BasicPackets_Test.py
+++ tests/BasicPackets_Test.py
@@ -10,15 +10,15 @@ class TestSimplePackets(unittest.TestCase):
     def setUp(self):
         class simple_pkt(models.Packet):
             field1 = models.IntField()
-            field2 = models.IntField()
+            field2 = models.IntField(unsigned=True)
 
         self.simple_pkt = simple_pkt
 
     def test_set_simple_int_fields(self):
         p = self.simple_pkt()
 
-        v1 = randint(0,100)
-        v2 = randint(0,100)
+        v1 = randint(-32768, 32767)
+        v2 = randint(0, 65536)
 
         p.field1 = v1
         p.field2 = v2
@@ -36,7 +36,7 @@ class TestSimplePackets(unittest.TestCase):
         p = self.simple_pkt()
         p2 = self.simple_pkt()
 
-        v1 = randint(0, 100)
+        v1 = randint(-32768, 32767)
         
         p.field1 = v1
 
@@ -44,23 +44,49 @@ class TestSimplePackets(unittest.TestCase):
 
         self.assertEqual(p.field1, p2.field1)
 
-    def test_set_array_field(self):
-        p = self.simple_pkt()
-
-        t_array = [randint(0,100) for i in range(10)]
-
-        p.field3 = t_array
-        self.assertEqual(p.field3, t_array)
-
 
     def test_from_binary(self):
-        vals = [randint(0, 100) for i in range(2)]
-        b_val = struct.pack('<' + 'h' * 2, *vals)
+        v1 = randint(-32768, 32767)
+        v2 = randint(0, 65536)
+        vals = [v1, v2]
+        b_val = struct.pack('hH', *vals)
 
         p = self.simple_pkt.from_bytes(b_val)
 
         self.assertEquals(p.field1, vals[0])
         self.assertEquals(p.field2, vals[1])
+
+    def test_to_binary(self):
+        v1 = randint(-32768, 32767)
+        v2 = randint(0, 65536)
+        vals = [v1, v2]
+        b_val = struct.pack('hH', *vals)
+
+        p = self.simple_pkt()
+        p.field1 = vals[0]
+        p.field2 = vals[1]
+
+        pkt_bin = p.to_bytes()
+
+        self.assertEquals(pkt_bin, b_val)
+
+    def test_to_from_binary(self):
+        vals = [randint(0, 100) for i in range(2)]
+
+        p = self.simple_pkt()
+        p.field1 = vals[0]
+        p.field2 = vals[1]
+
+        p2 = self.simple_pkt.from_bytes(p.to_bytes())
+
+        self.assertEquals(p.field1, p2.field1)
+        self.assertEquals(p.field2, p2.field2)
+
+
+    def test_invalid_key_words(self):
+        with self.assertRaises(KeyError):
+            class invalid_pkt(models.Packet):
+                inv_field = models.IntField(keyword_that_dont_exist=100)
 
 
 ## TDD: Prototyping for encapsulated packets.
