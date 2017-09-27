671ab08d59b384b05c6677d0d015b095a4999e6b
KronoSKoderS <mertz.james@gmail.com>
2017-09-27 14:15:42 -0700

    updated tests to be more readable

--- tests/BasicPackets_Test.py
+++ tests/BasicPackets_Test.py
@@ -8,90 +8,90 @@ from calpack import models
 
 class TestSimplePackets(unittest.TestCase):
     def setUp(self):
-        class simple_pkt(models.Packet):
-            field1 = models.IntField()
-            field2 = models.IntField(signed=True)
+        class two_int_field_packet(models.Packet):
+            int_field = models.IntField()
+            int_field_signed = models.IntField(signed=True)
 
-        self.simple_pkt = simple_pkt
+        self.two_int_field_packet = two_int_field_packet
 
     def test_set_simple_int_fields(self):
-        p = self.simple_pkt()
+        p = self.two_int_field_packet()
 
         v1 = randint(0, 65536)
         v2 = randint(-32768, 32767)
 
-        p.field1 = v1
-        p.field2 = v2
+        p.int_field = v1
+        p.int_field_signed = v2
 
-        self.assertEqual(p.field1, v1)
-        self.assertEqual(p.field2, v2)
+        self.assertEqual(p.int_field, v1)
+        self.assertEqual(p.int_field_signed, v2)
 
-    def test_set_invalid_int_field(self):
-        p = self.simple_pkt()
+    def test_field_raises_TypeError_when_setting_non_int_value(self):
+        p = self.two_int_field_packet()
         
         with self.assertRaises(TypeError):
-            p.field1 = ""
+            p.int_field = ""
 
     def test_set_valid_from_other_field(self):
-        p = self.simple_pkt()
-        p2 = self.simple_pkt()
+        p = self.two_int_field_packet()
+        p2 = self.two_int_field_packet()
 
         v1 = randint(0, 65536)
         
-        p.field1 = v1
+        p.int_field = v1
 
-        p2.field1 = p.field1
+        p2.int_field = p.int_field
 
-        self.assertEqual(p.field1, p2.field1)
+        self.assertEqual(p.int_field, p2.int_field)
 
 
-    def test_from_binary(self):
+    def test_create_class_from_bytes_string(self):
         v1 = randint(0, 65536)
         v2 = randint(-32768, 32767)
         vals = [v1, v2]
         b_val = struct.pack('Hh', *vals)
 
-        p = self.simple_pkt.from_bytes(b_val)
+        p = self.two_int_field_packet.from_bytes(b_val)
 
-        self.assertEquals(p.field1, vals[0])
-        self.assertEquals(p.field2, vals[1])
+        self.assertEquals(p.int_field, vals[0])
+        self.assertEquals(p.int_field_signed, vals[1])
 
-    def test_to_binary(self):
+    def test_export_to_bytes_string(self):
         v1 = randint(0, 65536)
         v2 = randint(-32768, 32767)
         vals = [v1, v2]
         b_val = struct.pack('Hh', *vals)
 
-        p = self.simple_pkt()
-        p.field1 = vals[0]
-        p.field2 = vals[1]
+        p = self.two_int_field_packet()
+        p.int_field = vals[0]
+        p.int_field_signed = vals[1]
 
         pkt_bin = p.to_bytes()
 
         self.assertEquals(pkt_bin, b_val)
 
-    def test_to_from_binary(self):
+    def test_to_bytes_string_then_import_from_binary(self):
         v1 = randint(0, 65536)
         v2 = randint(-32768, 32767)
         vals = [v1, v2]
 
-        p = self.simple_pkt()
-        p.field1 = vals[0]
-        p.field2 = vals[1]
+        p = self.two_int_field_packet()
+        p.int_field = vals[0]
+        p.int_field_signed = vals[1]
 
-        p2 = self.simple_pkt.from_bytes(p.to_bytes())
+        p2 = self.two_int_field_packet.from_bytes(p.to_bytes())
 
-        self.assertEquals(p.field1, p2.field1)
-        self.assertEquals(p.field2, p2.field2)
+        self.assertEquals(p.int_field, p2.int_field)
+        self.assertEquals(p.int_field_signed, p2.int_field_signed)
 
 
-    def test_invalid_key_words(self):
+    def test_class_raises_KeyError_when_using_invalid_key_words(self):
         with self.assertRaises(KeyError):
             class invalid_pkt(models.Packet):
                 inv_field = models.IntField(keyword_that_dont_exist=100)
 
     def test_check_word_size(self):
-        p = self.simple_pkt()
+        p = self.two_int_field_packet()
         self.assertEquals(p.num_words, 2)
 
 
