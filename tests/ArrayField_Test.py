492e4bb260204b6d5c340a59f4aa1fedfca86197
KronoSKoderS <mertz.james@gmail.com>
2017-11-13 12:52:41 -0700

    Added a test for individually getting a member of the ArrayField.

--- tests/ArrayField_Test.py
+++ tests/ArrayField_Test.py
@@ -8,10 +8,10 @@ from calpack import models
 
 
 class Test_ArrayField(unittest.TestCase):
-    def test_arrayfield_create_packet_with_multi_field(self):
+    def test_arrayfield_create_packet_with_array_field(self):
 
         class multi_int_field_packet(models.Packet):
-            list_int_field = models.ArrayField(models.IntField(), 10)
+            list_int_field = models.ArrayField(models.IntField, 10)
 
         expected_vals = list(range(10))
         p = multi_int_field_packet()
@@ -24,13 +24,25 @@ class Test_ArrayField(unittest.TestCase):
     def test_arrayfield_set_invalid_type_multi_field(self):
 
         class multi_int_field_packet(models.Packet):
-            list_int_field = models.ArrayField(models.IntField(), 10)
+            list_int_field = models.ArrayField(models.IntField, 10)
 
         expected_vals = list(range(10))
         p = multi_int_field_packet()
 
         with self.assertRaises(TypeError):
             p.list_int_field = 100
+
+
+    def test_arrayfield_access_individual_members(self):
+        class multi_int_field_packet(models.Packet):
+            list_int_field = models.ArrayField(models.IntField, 10)
+
+        expected_vals = list(range(10))
+        p = multi_int_field_packet()
+        p.list_int_field = expected_vals
+
+        for i, val in enumerate(expected_vals):
+            self.assertEquals(p.list_int_field[i], val)
 
 if __name__ == '__main__':
     unittest.main()
