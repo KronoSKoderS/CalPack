0145363da06a704e418b40dbd40620536d03f217
KronoSKoderS <mertz.james@gmail.com>
2017-11-27 01:36:34 -0700

    Adding test of more advanced packet (with multiple field types)

--- tests/AdvancedPackets_Test.py
+++ tests/AdvancedPackets_Test.py
@@ -5,6 +5,7 @@ from tests import PY2, PY3
 
 import unittest
 import struct
+import ctypes
 
 
 class Test_AdvancedPacket(unittest.TestCase):
@@ -71,7 +72,7 @@ class Test_AdvancedPacket(unittest.TestCase):
             self.assertEqual(pkt.points_two.points[i].y, 50)
             self.assertEqual(pkt._Packet__c_pkt.points_two.points[i].y, 50)
 
-    def test_verify_AttributeError_invalid_field_name_set_c_field(self):
+    def test_advpkt_verify_AttributeError_invalid_field_name_set_c_field(self):
         class Point(models.Packet):
             x = models.IntField()
             y = models.IntField()
@@ -80,4 +81,70 @@ class Test_AdvancedPacket(unittest.TestCase):
         with self.assertRaises(AttributeError):
             p.set_c_field('bad_name', 100)
 
+    def test_advpkt_create_complex_packet(self):
+        """
+        This test creates a packet that contains all the known built-in fields and ensures that 
+        the packet is correctly created and parsed back in.
+        """
+
+        class Point(models.Packet):
+            x = models.IntField(bit_len=8)
+            y = models.IntField(bit_len=8)
+
+        class PrimaryPacket(models.Packet):
+            int_field = models.IntField()
+            float_field = models.FloatField()
+            double_field = models.DoubleField()
+            long_double_field = models.LongDoubleField()
+            flags_field = models.ArrayField(models.FlagField(), 16)
+            pkt_field = models.PacketField(Point)
+            bool_field = models.BoolField()
+
+        class c_Point(ctypes.Structure):
+            _fields_ = (
+                ('x', ctypes.c_uint8),
+                ('y', ctypes.c_uint8)
+            )
+
+        class c_PrimaryPacket(ctypes.Structure):
+            _fields_ = (
+                ('int_field', ctypes.c_uint16),
+                ('float_field', ctypes.c_float),
+                ('double_field', ctypes.c_double),
+                ('long_double_field', ctypes.c_longdouble),
+                ('flags_field', ctypes.c_int16),
+                ('pkt_field', c_Point),
+                ('bool_field', ctypes.c_bool)
+            )
+
+        pkt = PrimaryPacket(
+            int_field=3,
+            float_field=3.14,
+            double_field=-3.14,
+            long_double_field=123.456,
+            flags_field=[True] * 16,
+            bool_field=True
+        )
+
+        c_pkt = c_PrimaryPacket()
+        c_pkt.int_field = 3
+        c_pkt.float_field = 3.14
+        c_pkt.double_field = -3.14
+        c_pkt.long_double_field = 123.456
+        c_pkt.flags_field = 0xFF
+        c_pkt.bool_field = True
+
+        b_str = pkt.to_bytes()
+        c_b_str = ctypes.string_at(ctypes.addressof(c_pkt), ctypes.sizeof(c_pkt))
+
+        self.assertEqual(b_str, c_b_str)
+
+        parsed_pkt = PrimaryPacket.from_bytes(c_b_str)
+
+        self.assertEqual(parsed_pkt.int_field, 3)
+        self.assertAlmostEqual(parsed_pkt.float_field, 3.14, places=5)
+        self.assertAlmostEqual(parsed_pkt.double_field, -3.14, places=5)
+        self.assertAlmostEqual(parsed_pkt.long_double_field, 123.456, places=5)
+        self.assertEqual(parsed_pkt.flags_field, [True] * 16)
+        self.assertEqual(parsed_pkt.bool_field, True)
 
