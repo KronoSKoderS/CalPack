95fe1b9d1afbd8e9ac31e3471c8a0f03df3323e5
Kronos <superuser.kronos@gmail.com>
2017-12-01 15:50:33 -0700

    ArrayField Update

--- calpack/models/fields.py
+++ calpack/models/fields.py
@@ -254,7 +254,7 @@ class PacketField(Field):
 
 class ArrayField(Field):
     """
-    A custom field for handling an array of fields
+    A custom field for handling an array of fields.  Only tuples or other ArrayFields can be written to the 
 
     :param array_cls: a :code:`calpack.models.Field` subclass **object** that represent the Field
         the array will be filled with.
@@ -273,13 +273,19 @@ class ArrayField(Field):
         self.c_type = (array_cls_tuple[1] * self.array_size)
 
     def c_to_py(self, c_field):
-        return c_field[:]
+        return tuple(c_field[:])
 
     def py_to_c(self, val):
-        if not isinstance(val, ArrayField) and not isinstance(val, list):
+        if not isinstance(val, ArrayField) and not isinstance(val, tuple) and not isinstance(val, list):
             raise TypeError("Must be of type ArrayField or list")
 
+        if len(val) != self.array_size:
+            raise ValueError("The length of val must be {}!".format(self.array_size))
+
         return self.c_type(*val)
+
+    def __len__(self):
+        return self.array_size
 
 
 class FlagField(Field):
--- tests/ArrayField_Test.py
+++ tests/ArrayField_Test.py
@@ -6,75 +6,88 @@ class Test_ArrayField(unittest.TestCase):
     def test_arrayfield_create_packet_with_array_field(self):
 
         class multi_int_field_packet(models.Packet):
-            list_int_field = models.ArrayField(models.IntField(), 10)
+            arr_int_field = models.ArrayField(models.IntField(), 10)
 
-        expected_vals = list(range(10))
+        expected_vals = tuple(range(10))
         p = multi_int_field_packet()
-        p.list_int_field = expected_vals
+        p.arr_int_field = expected_vals
 
-        self.assertEqual(p.list_int_field, expected_vals)
-        self.assertEqual(p._Packet__c_pkt.list_int_field[:], expected_vals)
+        self.assertEqual(p.arr_int_field, expected_vals)
+        self.assertEqual(tuple(p._Packet__c_pkt.arr_int_field[:]), expected_vals)
 
     def test_arrayfield_set_invalid_type_multi_field(self):
 
         class multi_int_field_packet(models.Packet):
-            list_int_field = models.ArrayField(models.IntField(), 10)
+            arr_int_field = models.ArrayField(models.IntField(), 10)
 
         p = multi_int_field_packet()
 
         with self.assertRaises(TypeError):
-            p.list_int_field = 100
+            p.arr_int_field = 100
+
+    def test_arrayfield_raises_valueerror_invalid_val_size(self):
+        class multi_int_field_packet(models.Packet):
+            arr_int_field = models.ArrayField(models.IntField(), 10)
+
+        p = multi_int_field_packet()
+
+        with self.assertRaises(ValueError):
+            p.arr_int_field = list(range(11))
+
+        with self.assertRaises(ValueError):
+            p.arr_int_field = list(range(9))
 
     def test_arrayfield_access_individual_members(self):
         class multi_int_field_packet(models.Packet):
-            list_int_field = models.ArrayField(models.IntField(), 10)
+            arr_int_field = models.ArrayField(models.IntField(), 10)
 
-        expected_vals = list(range(10))
+        expected_vals = tuple(range(10))
         p = multi_int_field_packet()
-        p.list_int_field = expected_vals
+        p.arr_int_field = expected_vals
 
         for i, val in enumerate(expected_vals):
-            self.assertEqual(p.list_int_field[i], val)
-            self.assertEqual(p._Packet__c_pkt.list_int_field[i], val)
+            self.assertEqual(p.arr_int_field[i], val)
+            self.assertEqual(p._Packet__c_pkt.arr_int_field[i], val)
 
     def test_arrayfield_compare_two_arrayfields(self):
         class multi_int_field_packet(models.Packet):
-            list_int_field = models.ArrayField(models.IntField(), 10)
+            arr_int_field = models.ArrayField(models.IntField(), 10)
 
         class multi_int_field_packet2(models.Packet):
-            list_int_field = models.ArrayField(models.IntField(), 10)
+            arr_int_field = models.ArrayField(models.IntField(), 10)
 
-        expected_vals = list(range(10))
+        expected_vals = tuple(range(10))
 
         p1 = multi_int_field_packet()
-        p1.list_int_field = expected_vals
+        p1.arr_int_field = expected_vals
 
         p2 = multi_int_field_packet2()
-        p2.list_int_field = list(reversed(expected_vals))
+        p2.arr_int_field = tuple(reversed(expected_vals))
 
-        self.assertNotEqual(p1.list_int_field, p2.list_int_field)
+        self.assertNotEqual(p1.arr_int_field, p2.arr_int_field)
 
-        p2.list_int_field = expected_vals
+        p2.arr_int_field = expected_vals
 
-        self.assertEqual(p1.list_int_field, p2.list_int_field)
+        self.assertEqual(p1.arr_int_field, p2.arr_int_field)
 
     def test_arrayfield_set_val_from_other_arrayfield(self):
         class multi_int_field_packet(models.Packet):
-            list_int_field = models.ArrayField(models.IntField(), 10)
+            arr_int_field = models.ArrayField(models.IntField(), 10)
 
-        expected_vals = list(range(10))
+        expected_vals = tuple(range(10))
 
-        p1 = multi_int_field_packet(list_int_field=expected_vals)
+        p1 = multi_int_field_packet(arr_int_field=expected_vals)
 
         p2 = multi_int_field_packet()
-        p2.list_int_field = p1.list_int_field
+        p2.arr_int_field = p1.arr_int_field
 
-        self.assertEquals(p2.list_int_field, expected_vals)
+        self.assertEquals(p2.arr_int_field, expected_vals)
 
     def test_arrayfield_raise_typeerror_non_byte_aligned_int_field(self):
         with self.assertRaises(TypeError):
             class multi_int_field(models.Packet):
-                list_int_field = models.ArrayField(models.IntField(bit_len=4), 10)
+                arr_int_field = models.ArrayField(models.IntField(bit_len=4), 10)
+
 
 if __name__ == '__main__':
     unittest.main()
