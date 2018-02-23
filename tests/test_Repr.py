49f133a73aa5d8d5983182525e94f9d87b50a365
Kronos <mertz.james@gmail.com>
2018-02-23 00:29:24 -0700

    pypy support.

--- tests/test_Repr.py
+++ tests/test_Repr.py
@@ -1,7 +1,7 @@
 import unittest
 
 from calpack import models
-from calpack.utils import PY2
+from calpack.utils import PY2, PYPY
 
 
 class Test_Repr(unittest.TestCase):
@@ -15,11 +15,11 @@ class Test_Repr(unittest.TestCase):
 
         expected_py3 = "two_int_pkt(field1=0, field2=0)"
         expected_py2 = "two_int_pkt(field1=0L, field2=0L)"
-        self.assertEqual(repr(pkt), expected_py2 if PY2 else expected_py3)
+        self.assertEqual(repr(pkt), expected_py2 if PY2 and not PYPY else expected_py3)
 
         pkt.field1 = 1234
         pkt.field2 = 5678
 
         expected_py3 = "two_int_pkt(field1=1234, field2=5678)"
         expected_py2 = "two_int_pkt(field1=1234L, field2=5678L)"
-        self.assertEqual(repr(pkt), expected_py2 if PY2 else expected_py3)
+        self.assertEqual(repr(pkt), expected_py2 if PY2 and not PYPY else expected_py3)
